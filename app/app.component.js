fa360448faefee710286b7962a64467a57bdf35f
clementGilardy <gilardy.clement@gmail.com>
2016-09-05 15:01:42 +0200

    add part routing

--- app/app.component.js
+++ app/app.component.js
@@ -9,29 +9,16 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var hero_service_1 = require('./hero.service');
 var AppComponent = (function () {
-    function AppComponent(heroService) {
-        this.heroService = heroService;
-        this.title = 'Tour of heroes';
+    function AppComponent() {
+        this.title = 'Tour of Heroes';
     }
-    AppComponent.prototype.onSelect = function (hero) {
-        this.selectedHero = hero;
-    };
-    AppComponent.prototype.getHeroes = function () {
-        var _this = this;
-        this.heroService.getHeroes().then(function (heroes) { return _this.heroes = heroes; });
-    };
-    AppComponent.prototype.ngOnInit = function () {
-        this.getHeroes();
-    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h2>My Heroes</h2> <ul class="heroes"><li *ngFor="let hero of heroes" [class.selected]="hero === selectedHero" (click)="onSelect(hero)"> <span class="badge">{{hero.id}}</span> {{hero.name}} </li> </ul> <my-hero-detail [hero]="selectedHero"></my-hero-detail>',
-            providers: [hero_service_1.HeroService]
+            template: "\n    <h1>{{title}}</h1>\n    <nav>\n      <a routerLink=\"/dashboard\" routerLinkActive=\"active\">Dashboard</a>\n      <a routerLink=\"/heroes\" routerLinkActive=\"active\">Heroes</a>\n    </nav>\n    <router-outlet></router-outlet>\n  "
         }), 
-        __metadata('design:paramtypes', [hero_service_1.HeroService])
+        __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
 }());
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,32 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h2>My Heroes</h2> <ul class="heroes"><li *ngFor="let hero of heroes" [class.selected]="hero === selectedHero" (click)="onSelect(hero)"> <span class="badge">{{hero.id}}</span> {{hero.name}} </li> </ul> <my-hero-detail [hero]="selectedHero"></my-hero-detail>',
-  providers: [HeroService]
+  template: `
+    <h1>{{title}}</h1>
+    <nav>
+      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `
 })
-
-export class AppComponent implements OnInit {
-  title = 'Tour of heroes';
-  heroes : Hero[];
-  selectedHero: Hero;
-
-  constructor(private heroService: HeroService){}
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-
-  ngOnInit(): void {
-    this.getHeroes();
-  }
+export class AppComponent {
+  title = 'Tour of Heroes';
 }
-
-
-
-
